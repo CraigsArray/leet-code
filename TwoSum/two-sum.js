a8aae7a552d626c2480f4bad007fedc587edfcba
Craig M Lewis <102835644+CraigsArray@users.noreply.github.com>
2022-07-26 17:45:46 -0700

    Fixed small error
    j needs to start and the next index else the solution could be the first index added with itself...only compare different elements.

--- TwoSum/two-sum.js
+++ TwoSum/two-sum.js
@@ -1,7 +1,7 @@
 //First solution that came to mind. Nested for loops allow us to search through the entire array testing each index with all other indeces.
 var twoSum = function(nums, target) {
     for(var i = 0; i < nums.length; i++){
-        for(var j = 0; j < nums.length; j++){
+        for(var j = i + 1; j < nums.length; j++){
             if(nums[i] + nums[j] == target) return [i,j]
         }
     }
