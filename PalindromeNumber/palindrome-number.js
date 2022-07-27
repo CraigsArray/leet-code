7a168c6721b075b93d3f5c7aea12ddbb18cfd929
Craig M Lewis <clewis1337@gmail.com>
2022-07-26 18:10:56 -0700

    Slightly faster
    Sped it up by exiting early if i and j are both pointing to the middle number

--- PalindromeNumber/palindrome-number.js
+++ PalindromeNumber/palindrome-number.js
@@ -4,6 +4,7 @@
  */
 // One for loop means an O(n) runtime however solution is slow as toString() can be very taxing.
 // Summary: Convert number to a string, then have i start as the leftmost number, and j be rightmost number, increment i and decrement j as you move towards the number in the middle
+// Revisit: Can speed up by returning early with both these counters are looking at the middle number and haven't encountered any false pairs.
  var isPalindrome = function(x) {
     if(x < 0) return false
     else{
@@ -12,8 +13,8 @@
         for(i = 0; i < xString.length; i++){
             if(xString.charAt(i) !== xString.charAt(j)) return false
             j--;
-
+            if(i == j) return true
         }
-        return true
+       return true
     }
 };
