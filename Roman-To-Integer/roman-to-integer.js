5dec63fa3d91345c3b7d996989650ebad17d3d3c
Craig M Lewis <clewis1337@gmail.com>
2022-07-29 18:44:05 -0700

    Addeded roman-to-int
    Used more if statements than I would prefer. Will revisit.

---
+++ Roman-To-Integer/roman-to-integer.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
+    romanNumMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000            
+    }
+    var output = 0;
+    for(i = 0; i < s.length; i++){
+        if(s.charAt(i) == "I" && s.charAt(i+1) == "V"){
+                output += 4
+                i++
+            }
+        else if(s.charAt(i) == "I" && s.charAt(i+1) == "X"){
+                output += 9
+                i++
+            }         
+        else if(s.charAt(i) == "X" && s.charAt(i+1) == "L") {
+                output += 40
+                i++
+            }
+        else if(s.charAt(i) == "X" && s.charAt(i+1) == "C") {
+                output += 90
+                i++
+            }
+        else if(s.charAt(i) == "C" && s.charAt(i+1) == "D") {
+                output += 400
+                i++
+            }
+        else if(s.charAt(i) == "C" && s.charAt(i+1) == "M") {
+                output += 900
+                i++
+            }
+        else output += romanNumMap[s.charAt(i)]
+    }
+    return output
+};
