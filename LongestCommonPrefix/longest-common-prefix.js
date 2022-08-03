489ef94ae43b8278eed755f1087afed8fe1d6ef3
Craig M Lewis <clewis1337@gmail.com>
2022-08-02 22:19:23 -0700

    Fixed minor errors and changed spacing

--- LongestCommonPrefix/longest-common-prefix.js
+++ LongestCommonPrefix/longest-common-prefix.js
@@ -1,10 +1,8 @@
 var longestCommonPrefix = function(strs) {
-    //Base check 
-    if(strs.length < 2) {
+    if(strs.length < 2) { //Edge case - only 1 string input
         return strs[0];
     }
-    //helper function which return the common prefix between two
-    const matchTwoStr = (str1,str2)=>{
+    const matchStrings = function(str1, str2){
         let matched = "";
         let counter = 0;
         while(counter < str1.length || counter < str2.length) {
@@ -18,15 +16,15 @@ var longestCommonPrefix = function(strs) {
         return matched;
     }
     //Set up a base common prefix which we will build upon
-    let base = matchTwoStr(strs[0], strs[1]);
+    let base = matchStrings(strs[0], strs[1]);
     if(strs.length ===2) {
         return base;
     }
     let result = "";
     for(let i = 2;i < strs.length;i++) { 
-        result = matchTwoStr(base, strs[i]);
+        result = matchStrings(base, strs[i]);
         //update the base common prefix on every iteration
-        base = matchTwoStr(base, strs[i])
+        base = matchStrings(base, strs[i])
     }
     return result;
 };
