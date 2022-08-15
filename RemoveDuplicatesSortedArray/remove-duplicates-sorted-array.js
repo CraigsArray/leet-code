0c02d94a624c0220130b204d84e0a337daf521e2
Craig M Lewis <clewis1337@gmail.com>
2022-08-14 19:58:06 -0700

    Create remove-duplicates-sorted-array.js

---
+++ RemoveDuplicatesSortedArray/remove-duplicates-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) { //we are comparing two numbers side by side
+        if (nums[j] != nums[i]){ //if 
+            i++;
+            nums[i] = nums[j];
+        } 
+    }
+    i++;
+    return i;
+};
