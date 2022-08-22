c4f8bf80958665f69905e41aaf15fb0c36c29ad1
Craig M Lewis <clewis1337@gmail.com>
2022-08-21 22:42:06 -0700

    Update remove-duplicates-sorted-array.js

--- RemoveDuplicatesSortedArray/remove-duplicates-sorted-array.js
+++ RemoveDuplicatesSortedArray/remove-duplicates-sorted-array.js
@@ -5,9 +5,9 @@
  var removeDuplicates = function(nums) {
     let i = 0;
     for (let j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]){ //cycle through the array, and verify that 2 adjacent numbers are not equal
+        if (nums[j] != nums[i]){            //cycle through the array, and verify that 2 adjacent numbers are not equal
             i++;
-            nums[i] = nums[j];
+            nums[i] = nums[j];              //Only add unique values
         } 
     }
     i++;
