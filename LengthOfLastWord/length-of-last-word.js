0d569d10e969df6092532a55cec70d78b845fdac
Craig M Lewis <clewis1337@gmail.com>
2022-09-01 15:40:31 -0700

    Update length-of-last-word.js

--- LengthOfLastWord/length-of-last-word.js
+++ LengthOfLastWord/length-of-last-word.js
@@ -6,7 +6,7 @@
     if (s.length === 0) return; //edge case
     
     let lastWord = "";
-    for (let i = s.length - 1; i > 0; i--){ //start from the end
+    for (let i = s.length - 1; i >= 0; i--){ //start from the end
         if(s[i] !== " ") lastWord += s[i]; //build our last word backwards
         if(s[i] === " " && lastWord !== "") break; //once we started filling up our word and encounter another, break
     }
