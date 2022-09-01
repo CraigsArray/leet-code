37634bf596d2baa52b0106da7b3eb5c600401168
Craig M Lewis <clewis1337@gmail.com>
2022-08-31 20:51:26 -0700

    Update search-insert-position.js

--- SearchInsertPosition/search-insert-position.js
+++ SearchInsertPosition/search-insert-position.js
@@ -3,11 +3,12 @@
  * @param {number} target
  * @return {number}
  */
- var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] === target) return i;
-        if(nums[i] < target && (nums[i + 1] > target || i + 1 === nums.length)) return i + 1;
-    }
-    return 0;
+// O(n) Solution
+//  var searchInsert = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] === target) return i;
+//         if(nums[i] < target && (nums[i + 1] > target || i + 1 === nums.length)) return i + 1;
+//     }
+//     return 0;
     
-};
+// };
