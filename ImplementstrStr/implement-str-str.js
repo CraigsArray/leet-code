14a7052efb5176fd03a3f22a42f10aa6aec3fe45
Craig M Lewis <clewis1337@gmail.com>
2022-08-31 20:37:07 -0700

    Update implement-str-str.js

--- ImplementstrStr/implement-str-str.js
+++ ImplementstrStr/implement-str-str.js
@@ -5,15 +5,13 @@
  */
  var strStr = function(haystack, needle) {
     if(needle.length === 0) return 0
+    let j = 0;
     for(let i=0;i<haystack.length;i++){
-        if(haystack[i] === needle[0]){
-            for(let j = 0; j < needle.length; j++){
-                if(haystack[i + j] !== needle[j]) break
-                if(j === needle.length - 1) return i;
-            }
+        while(haystack[i + j] === needle[j]){  //Treat as an if statement that looks at the starting letter of needle to the last letter when it finds it
+            if(j === needle.length - 1) return i; //we found all the letters!
+            j++;
         }
-
-
+        j = 0;      //didn't find letters, reset our counter to zero to look at first letter again
     }
     return -1;
 };
