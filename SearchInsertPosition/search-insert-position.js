75f58b208cb3acdd946f4ecf2c3053a583ba07a7
Craig M Lewis <clewis1337@gmail.com>
2022-08-31 21:16:53 -0700

    Update search-insert-position.js

--- SearchInsertPosition/search-insert-position.js
+++ SearchInsertPosition/search-insert-position.js
@@ -14,17 +14,20 @@
 // };
 
 // O(log n) solution using BST
- var binarySearchInsert = function(nums, target) {
+var searchInsert = function(nums, target) {
     if(nums == null || nums.length == 0) return 0; //edge cases
     
     let n = nums.length; //set up variables for left / right
 	let l = 0;
 	let r = n - 1;
 
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] === target) return i;
-        if(nums[i] < target && (nums[i + 1] > target || i + 1 === nums.length)) return i + 1;
+    while(l < r){
+        let m = Math.floor((l + (r - 1)) / 2);
+        
+        if(nums[m] == target) return m; //Lucky us, it was in the middle!
+        else if (nums[m] < target) l = m + 1; //make new left just right of center
+        else r = m - 1;  //make right just left of center
     }
-    return 0;
-    
+    if (nums[l] < target) return l  + 1; //end result was right number
+    else return l;  //end result was left number
 };
