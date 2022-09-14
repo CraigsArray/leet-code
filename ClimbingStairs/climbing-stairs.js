4c6b98a421400a6faa522be73f197708ba300441
Craig M Lewis <clewis1337@gmail.com>
2022-09-14 15:40:47 -0700

    updated

--- ClimbingStairs/climbing-stairs.js
+++ ClimbingStairs/climbing-stairs.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
  var climbStairs = function(n) {
-    
-    let ways = [n + 1]; 
-    ways[0] = 1;
+    //Fibonacci - each step can be reached by the step before it, or two steps before it. Add those steps together
+    let ways = [n]; //staircase is at least n long
+    ways[0] = 1; //First 2 steps only have 1 way to get to them...
     ways[1] = 1;
+    //Every step after those will be a sum of the ways to get to the 2 steps before it
     for(let i = 2; i <= n; i++) ways[i] = ways[i - 1] + ways[i - 2];
     return ways[n];
 };
--- TwoSum/two-sum.js
+++ TwoSum/two-sum.js
@@ -1,8 +1,22 @@
 //First solution that came to mind. Nested for loops allow us to search through the entire array testing each index with all other indeces.
+//Brute force O(n^2)
 var twoSum = function(nums, target) {
-    for(var i = 0; i < nums.length; i++){
+    for(var i = 0; i < nums.length; i++){  //Brute force means we are comparing every number against every other number...
         for(var j = i + 1; j < nums.length; j++){
             if(nums[i] + nums[j] == target) return [i,j]
         }
     }
 };
+
+//Hash map O(n)
+var twoSumHash = function(nums, target){
+    const map = {};
+
+    for(let i = 0; i < nums.length; i++){
+        const pairedNum = target - nums[i];  //pairedNum is a num where nums[i] + pairedNum = target
+                                             //we query our map's keys to see if the number exists already
+        if(map[pairedNum] !== null) return [map[pairedNum], i]  //we found pairedNum! Return both indeces
+        map[nums[i]] = i;  //we didn't find it. curr number = nums[i]. We save the index, i, in map where key = number                      
+    }
+    return;
+}
