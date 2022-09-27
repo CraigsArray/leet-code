e755511a41aead1f5338a7f19945b5e39c22fcc7
Craig M Lewis <clewis1337@gmail.com>
2022-09-27 15:01:29 -0700

    Update happy-number.js

--- HappyNumber/happy-number.js
+++ HappyNumber/happy-number.js
@@ -1,5 +1,5 @@
 /**
- * Write an algorithm to determine if a number n is happy.
+Write an algorithm to determine if a number n is happy.
 A happy number is a number defined by the following process:
     1 Starting with any positive integer, replace the number by the sum of the squares of its digits.
     2 Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
