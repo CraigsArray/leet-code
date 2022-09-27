93fa9fc4d1363a53b15abb0cc1b45ddde81b118a
Craig M Lewis <clewis1337@gmail.com>
2022-09-26 20:01:09 -0700

    Update read-n-characters-given-read4.js

--- ReadNCharactersGivenRead4/read-n-characters-given-read4.js
+++ ReadNCharactersGivenRead4/read-n-characters-given-read4.js
@@ -12,9 +12,6 @@ Method read4:
 The API read4 reads four consecutive characters from file, then writes those characters into the buffer array buf4.
 The return value is the number of actual characters read.
 Note that read4() has its own file pointer, much like FILE *fp in C.
- */
-
-/**
  * @param {function} read4()
  * @return {function}
  */
