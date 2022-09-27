23f4eb65abfaa307b6d7a583236244af49ac74ca
Craig M Lewis <clewis1337@gmail.com>
2022-09-26 20:01:17 -0700

    Update read-n-characters-given-read4.js

--- ReadNCharactersGivenRead4/read-n-characters-given-read4.js
+++ ReadNCharactersGivenRead4/read-n-characters-given-read4.js
@@ -5,8 +5,7 @@
  * @return {number} The number of actual characters read
  * read4 = function(buf4) {
  *     ...
- * };
- * 
+ * }; 
  * Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
 Method read4:
 The API read4 reads four consecutive characters from file, then writes those characters into the buffer array buf4.
