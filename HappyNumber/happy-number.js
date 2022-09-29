55c39a55927531886620b1e015df0fe59d96f572
Craig M Lewis <clewis1337@gmail.com>
2022-09-28 22:19:14 -0700

    Update happy-number.js

--- HappyNumber/happy-number.js
+++ HappyNumber/happy-number.js
@@ -11,8 +11,7 @@ Return true if n is a happy number, and false if not.
  var isHappy = function(n) {
     let sum
     const sumSet = new Set();  //hold all sums encountered
-    while(true){  //indefinite loop
-        
+    while(true){  //indefinite loop        
         n = n.toString();  //easy way to separatae digits (could use % for faster speed but more convoluted)
         sum = 0;
         for(let i = n.length - 1; i >= 0; i--){ //take each digit, square it, add to sum
