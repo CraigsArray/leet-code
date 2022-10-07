4427041375f7e248d8bdbc3396aaa6f4e1953929
Craig M Lewis <clewis1337@gmail.com>
2022-10-06 20:01:32 -0700

    Update isomorphic-strings.js

--- IsomorphicStrings/isomorphic-strings.js
+++ IsomorphicStrings/isomorphic-strings.js
@@ -2,6 +2,10 @@
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+ * 
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
  */
  var isIsomorphic = function(s, t) {
     const sMap = new Map();  //2 maps
