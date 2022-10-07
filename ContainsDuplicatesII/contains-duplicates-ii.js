3b89d2c6760131cb69527294258d6fdca8b6e5e8
Craig M Lewis <clewis1337@gmail.com>
2022-10-07 16:42:56 -0700

    Create contains-duplicates-ii.js

---
+++ ContainsDuplicatesII/contains-duplicates-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+ var containsNearbyDuplicate = function(nums, k) {
+    let seenMap = new Map();  //map is great here because RAM has a runtime of O(1) and we store both values and indicies
+    for(let i = 0; i < nums.length; i++){
+
+        if(seenMap.has(nums[i])){  //number was seen
+            let j = seenMap.get(nums[i]);  //get the index of prev time num was seen
+            if(Math.abs(i - j) <= k) return true;  //true condition
+        } 
+        seenMap.set(nums[i], i);  //we want to continually replace the index of the last time the num was seen
+                                  //so that the difference between two occurences is as small as possible        
+    }
+    return false;  //never hit true condition
+};
