e628d2b818b019312b20ec23efcd75aec95e95b9
Craig M Lewis <clewis1337@gmail.com>
2022-10-07 16:43:38 -0700

    Update contains-duplicates-ii.js

--- ContainsDuplicatesII/contains-duplicates-ii.js
+++ ContainsDuplicatesII/contains-duplicates-ii.js
@@ -2,7 +2,9 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
+ * Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
  */
+ //Runtime: O(n) - loop through array once.
  var containsNearbyDuplicate = function(nums, k) {
     let seenMap = new Map();  //map is great here because RAM has a runtime of O(1) and we store both values and indicies
     for(let i = 0; i < nums.length; i++){
