e3c8daa1f6b19521f5e42e1077a2c1d3f9b6c7cd
Craig M Lewis <clewis1337@gmail.com>
2022-10-07 16:24:55 -0700

    Update contains-duplicates.js

--- ContainsDuplicates/contains-duplicates.js
+++ ContainsDuplicates/contains-duplicates.js
@@ -1,12 +1,14 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
  */
+ //Runtime: O(n) - loop through array once.
  var containsDuplicate = function(nums) {
-    let seenSet = new Set();
+    let seenSet = new Set();  //set is great here because RAM has a runtime of O(1)
     for(let i = 0; i < nums.length; i++){
-        if(seenSet.has(nums[i])) return true;
-        seenSet.add(nums[i]);
+        if(seenSet.has(nums[i])) return true;  //Check if already in set, O(1)
+        seenSet.add(nums[i]);  //add to set
     }
-    return false;
+    return false;  //never hit a duplicate
 };
