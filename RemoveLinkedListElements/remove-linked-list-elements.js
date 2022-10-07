4f061c4877c34c3a6dd2043df12fd5132458c1f0
Craig M Lewis <clewis1337@gmail.com>
2022-10-06 17:43:23 -0700

    Update remove-linked-list-elements.js

--- RemoveLinkedListElements/remove-linked-list-elements.js
+++ RemoveLinkedListElements/remove-linked-list-elements.js
@@ -8,8 +8,7 @@
 /**
  * @param {ListNode} head
  * @param {number} val
- * @return {ListNode}
- 
+ * @return {ListNode} 
    Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
    //Runtime - O(n). Loop through the size of input once.
  */
