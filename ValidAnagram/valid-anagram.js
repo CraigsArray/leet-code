0934c58565b74c8b74c0683f91fead6cffcf397d
Craig M Lewis <clewis1337@gmail.com>
2022-10-11 21:42:53 -0700

    Update valid-anagram.js

--- ValidAnagram/valid-anagram.js
+++ ValidAnagram/valid-anagram.js
@@ -6,22 +6,23 @@
  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
  */
  //Runtime: O(n)
+ //Explanation: We use a map to count the occurences of letters in S loop. Then we remove them in T loop.
  var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;  //needs to be true
     
-    let seen = new Map();
+    let seen = new Map();  //Holds letters and the number of times they occur
     for(let i = 0; i < s.length; i++){
-        if(seen[s[i]] === undefined) seen[s[i]] = 1;
+        if(seen[s[i]] === undefined) seen[s[i]] = 1;  //First occurence, add it to map and set occurences to 1
         else{
-            seen[s[i]]++;
+            seen[s[i]]++;   //Already seen, increment occurences
         }
     }
     
     for(let i = 0; i < t.length; i++){
-        if(seen[t[i]] === undefined) return false;
-        if(seen[t[i]] === 1) seen[t[i]] = undefined;
+        if(seen[t[i]] === undefined) return false;  //Ruh roh, we can't pop the letter. Return false!
+        if(seen[t[i]] === 1) seen[t[i]] = undefined;  //Only one occurence remaining, remove it from the list
         else{
-            seen[t[i]]--;
+            seen[t[i]]--; //Multiple occurences remaining, decrement by 1
         }
     }
     
