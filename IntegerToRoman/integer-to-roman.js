c0908bcc0f5471e465ccd283ea124f6acbc84c11
Craig M Lewis <clewis1337@gmail.com>
2022-10-12 21:38:05 -0700

    Update integer-to-roman.js

--- IntegerToRoman/integer-to-roman.js
+++ IntegerToRoman/integer-to-roman.js
@@ -12,13 +12,13 @@
     M             1000
  */
  //Runtime O(1);
- //Summary: Just gradually reducing the number. While loops occur when a letter can be repeated
+ //Summary: Just gradually reducing the number. 
  var intToRoman = function(num) {
     let output = "";
     const map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
-    Object.entries(map).forEach(([letter, value]) => {
-        while(num >= value){
-            output += letter;
+    Object.entries(map).forEach(([letter, value]) => {  //each element is a pair, deconstruct them into each variable
+        while(num >= value){   //if num is bigger than value
+            output += letter;  //add the letter and reduce it by that value
             num -= value;
         }
     });
@@ -26,6 +26,7 @@
 };
 
  //Long version - less memory
+ //While loops occur when a letter can be repeated
  /*
  var intToRoman = function(num) {
     let output = "";
