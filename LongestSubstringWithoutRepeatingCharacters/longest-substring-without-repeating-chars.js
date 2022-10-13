c5fbfda4ef3c8f87006d7de8f8a05de90a13ca5f
Craig M Lewis <clewis1337@gmail.com>
2022-10-12 19:36:04 -0700

    Update longest-substring-without-repeating-chars.js

--- LongestSubstringWithoutRepeatingCharacters/longest-substring-without-repeating-chars.js
+++ LongestSubstringWithoutRepeatingCharacters/longest-substring-without-repeating-chars.js
@@ -2,30 +2,27 @@
  * @param {string} s
  * @return {number}
  */
+ //Runtime: O(n*logn)?
+ //A bit on the slow side as we reset fully each time, creating overlap. Can be improved.
  var lengthOfLongestSubstring = function(s) {
     if (s.length < 1) return 0;  //edge case
     if (s.length === 1) return 1;  //edge case
     if (s.length === 2 && s[0] !== s[1]) return 2; //edge case
-    let map = {};
-    let subString = "";
-    let j = 0;
-    
-   
+    let sizes = [];
+    let substring = "";
+    let j = 0;  //Holds the start index of current substring
     for(let i = 0; i < s.length; i++){
-        if(subString.includes(s[i])){
-            map[subString.length] = subString;
-            j++;
-            i = j;
-            subString = s[i];
+        if(substring.includes(s[i])){  //Repeat char
+            sizes.push(substring.length); //Add curr substring length to array
+            j++;    //Move start index up one
+            i = j;  //Reset our i
+            substring = s[i]; //Reset our substring
         }
         else{
-            subString += s[i];
+            substring += s[i];
         }
 
     }
-    map[subString.length] = subString
-    let ansArray = Object.keys(map);
-    let longest = parseInt(ansArray[ansArray.length - 1])
-    return longest;
-    
+    sizes.push(substring.length);  //Add final substring length
+    return Math.max(...sizes);  //return the max
 };
