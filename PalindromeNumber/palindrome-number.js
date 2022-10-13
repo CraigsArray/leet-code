269836bac1546b95c5e209466e770ba45822a9d2
Craig M Lewis <clewis1337@gmail.com>
2022-10-12 20:34:13 -0700

    Update palindrome-number.js

--- PalindromeNumber/palindrome-number.js
+++ PalindromeNumber/palindrome-number.js
@@ -2,21 +2,19 @@
  * @param {number} x
  * @return {boolean}
  * Given an integer x, return true if x is palindrome integer.
-An integer is a palindrome when it reads the same backward as forward.
+   An integer is a palindrome when it reads the same backward as forward.
  */
-// One for loop means an O(n) runtime however solution is slow as toString() can be very taxing.
+// Runtime: O(logn)? Moving in fromt front and back
 // Summary: Convert number to a string, then have i start as the leftmost number, and j be rightmost number, increment i and decrement j as you move towards the number in the middle
-// Revisit: Can speed up by returning early with both these counters are looking at the middle number and haven't encountered any false pairs.
- var isPalindrome = function(x) {
-    if(x < 0) return false
-        else{
-            var xString = x.toString()
-            j = xString.length - 1
-            for(i = 0; i < xString.length; i++){
-                if(xString.charAt(i) !== xString.charAt(j)) return false
-                j--;
-                if(i == j) return true
-        }
-       return true
+var isPalindrome = function(x) {
+    if(x < 0) return false;  
+    if(x === 1) return true;  //edgecases
+    var xString = x.toString();  //strings are easier to compare  
+    j = xString.length - 1;
+    for(i = 0; i <= j; i++){  //2 fingers moving towards each other, i moves from front, j from back
+        if(xString[i] != xString[j]) return false;  //at any point they don't match, return false
+        j--;  //move in from the back
     }
+    return true; //success
+        
 };
