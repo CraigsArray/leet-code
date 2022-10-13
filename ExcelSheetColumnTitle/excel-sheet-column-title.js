b7b247ef80a87ab8f00542be0a44cf4188167d9a
Craig M Lewis <clewis1337@gmail.com>
2022-10-12 17:46:05 -0700

    Create excel-sheet-column-title.js

---
+++ ExcelSheetColumnTitle/excel-sheet-column-title.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+ */
+ var convertToTitle = function(columnNumber) {
+    let alphabet = "ZABCDEFGHIJKLMNOPQRSTUVWXY";
+    let alphaMap = {};
+    for(let i = 0; i < 26; i++){
+        alphaMap[i] = alphabet[i];
+    }
+    let colTitle = "";
+    let power = 1;
+    while(columnNumber > 0){
+        let r = columnNumber % 26;
+        columnNumber = Math.floor(columnNumber / 26);
+        if(r === 0) columnNumber--;
+        colTitle = alphaMap[r] + colTitle;
+        
+    }
+    return colTitle;
+};
