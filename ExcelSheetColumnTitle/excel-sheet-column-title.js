b0cc7080feec17b118d4361556ca10d2bf6ac89a
Craig M Lewis <clewis1337@gmail.com>
2022-10-14 20:06:58 -0700

    Update excel-sheet-column-title.js

--- ExcelSheetColumnTitle/excel-sheet-column-title.js
+++ ExcelSheetColumnTitle/excel-sheet-column-title.js
@@ -1,7 +1,7 @@
 /**
  * @param {number} columnNumber
  * @return {string}
- Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+  Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
  */
  //Runtime: O(logn)? Might even be O(1)?
  var convertToTitle = function(columnNumber) {
@@ -13,7 +13,7 @@
     let colTitle = "";  //Title will be built from right to left
     while(columnNumber > 0){
         let r = columnNumber % 26;  //First get the remainder
-        columnNumber = Math.floor(columnNumber / 26);  //Numbers are in pseudo-base26, move to next order of magnitude AKA: shift left.
+        columnNumber = Math.floor(columnNumber / 26);  //move to next order of magnitude AKA: shift left.
         if(r === 0) columnNumber--;  //Z will only be the leftmost number. All other occurences of r%26===0 should by Y's
         colTitle = alphaMap[r] + colTitle;  //Build small to big, right to left.
         
