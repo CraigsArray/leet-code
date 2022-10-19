5fd1684be446ee336b9ba8a3f03de8edc836dbf4
Craig M Lewis <clewis1337@gmail.com>
2022-10-19 15:55:56 -0700

    Update balanced-binary-tree.js

--- BalancedBinaryTree/balanced-binary-tree.js
+++ BalancedBinaryTree/balanced-binary-tree.js
@@ -9,9 +9,13 @@
 /**
  * @param {TreeNode} root
  * @return {boolean}
+ * Given a binary tree, determine if it is height-balanced.
+   For this problem, a height-balanced binary tree is defined as:
+     a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+ * Runtime: O(n)
  */
  var isBalanced = function(root) {
-    var balanced = true;
+    var balanced = true;  //Flag
 
     var traverse = function(node, depthLeft, depthRight){
         if(!node) return null;    
