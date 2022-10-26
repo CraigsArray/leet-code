1c85f9882a249cc9ca040611ed74df37b7923515
Craig M Lewis <clewis1337@gmail.com>
2022-10-25 17:27:58 -0700

    Update reverse-integer.js

--- ReverseInteger/reverse-integer.js
+++ ReverseInteger/reverse-integer.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
  var reverse = function(x) {
-    let output = parseInt(x.toString().split("").reverse().join(""))
-    if(output > (2**31-1) || output < ((-2)**31)) return 0;
-    if(x < 0) return (output * -1);
+    let output = parseInt(x.toString().split("").reverse().join(""));  //toString, toArray, reverse, toString, toInt
+    if(output > (2**31-1) || output < ((-2)**31)) return 0;  //too big or small
+    if(x < 0) return (output * -1);  //get the sign
     return output;
 };
