d172ed7236ffc296b134749f0e478f2b7421cb84
Craig M Lewis <clewis1337@gmail.com>
2023-02-09 13:32:01 -0800

    Create ugly-number.js

---
+++ UglyNumber/ugly-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    //1 is an ugly number
+    if(n == 1) return true;
+    //Reduce the number to the prime conditions
+    while(n % 5 == 0 && n > 5){
+        n = n / 5
+    }
+    while(n % 3 == 0 && n > 3){
+        n = n / 3
+    }
+    while(n % 2 == 0 && n > 2){
+        n = n / 2
+    }
+    //Check to see if the primes match conditions
+    return (n == 2) || (n == 3) || (n == 5)
+};
