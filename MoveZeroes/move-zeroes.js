cf3e2a7d0e88d02aad92866bf9167c5d32e7ab22
Craig M Lewis <clewis1337@gmail.com>
2023-03-21 15:07:17 -0700

    Create move-zeroes.js

---
+++ MoveZeroes/move-zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var length = nums.length - 1
+    var i = 0
+    var zeroCount = 0
+    while(i < length){
+        if(nums[i] == 0) {
+            nums.splice(i, 1)
+            zeroCount++
+        }
+        else{
+            i++
+        }
+    }
+    while(zeroCount > 0) {
+        nums.push(0)
+        zeroCount--
+    }
+    return nums
+};
