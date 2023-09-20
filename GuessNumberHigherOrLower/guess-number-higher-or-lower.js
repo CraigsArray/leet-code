975090ad665f1d1eba404e842f2708da1e7127e8
Craig M Lewis <clewis1337@gmail.com>
2023-09-20 14:28:21 -0700

    Create guess-number-higher-or-lower.js

---
+++ GuessNumberHigherOrLower/guess-number-higher-or-lower.js
@@ -0,0 +1,36 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    var left = 1;
+    var right = n;
+    var curr = Math.floor((left + right)/2);
+
+    var binarySearch = function(curr, left, right){
+        //Base Condition
+        if(left > right) return false;
+        //Found Answer
+        if(guess(curr) === 0) return curr;
+        //New search, new mid
+        var curr = Math.floor((left + right)/2);
+        //Too big, change limit on right
+        if(guess(curr) === -1) binarySearch(curr, left, curr - 1);
+        //Too small, change limit on left
+        if(guess(curr) === 1) binarySearch(curr, curr + 1, right);
+ 
+    }
+
+   var output = binarySearch(curr, left, right);
+   return output;
+    
+};
